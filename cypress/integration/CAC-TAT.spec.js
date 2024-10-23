da6f5a2b8802d49f8e547ba84b36a68a81818fc4
Geraldo Miguel Zys Júnior <geraldo.zys@sistran.com.br>
2024-10-23 09:12:06 -0300

    teste

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,85 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function() {
         cy.visit('./src/index.html')
+    })
 
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
+
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = '123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789'
+        cy.get('#firstName').type('Teste')
+        cy.get('#lastName').type('Teste')
+        cy.get('#email').type('teste@teste.com')
+        cy.get('#open-text-area').type(longText, { delay: 0})
+        cy.contains('button','Enviar').click()
+
+        cy.get('.success').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        cy.get('#firstName').type('Teste')
+        cy.get('#lastName').type('Teste')
+        cy.get('#email').type('teste@testecom')
+        cy.get('#open-text-area').type('teste')
+        cy.contains('button','Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
+        cy.get('#phone')
+            .type('abcdefghij')
+            .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('#firstName').type('Teste')
+        cy.get('#lastName').type('Teste')
+        cy.get('#email').type('teste@teste.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('teste')
+        cy.contains('button','Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('#firstName')
+            .type('Teste')
+            .should('have.value', 'Teste')
+            .clear()
+            .should('have.value', '')
+        cy.get('#lastName')
+            .type('Teste')
+            .should('have.value', 'Teste')
+            .clear()
+            .should('have.value', '')
+        cy.get('#email')
+            .type('teste@teste.com')
+            .should('have.value', 'teste@teste.com')
+            .clear()
+            .should('have.value', '')
+        cy.get('#phone')
+            .type('123456')
+            .should('have.value', '123456')
+            .clear()
+            .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.contains('button','Enviar').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it ('envia o formuário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSubmit()
+
+        cy.get('.success').should('be.visible')
+    })
+
   })
   
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
+    cy.get('#firstName').type('Teste')
+    cy.get('#lastName').type('Teste')
+    cy.get('#email').type('teste@teste.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button','Enviar').click()
+})
